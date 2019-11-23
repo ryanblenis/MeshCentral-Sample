4c3bb7b99f1c2cb5d41840e722a3dbc801f06989
Ryan Blenis <ryan.blenis@gmail.com>
2019-11-23 03:17:06 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+# MeshCentral-Sample
+
+*Released: 2019-11-23*
+
+Sample plugin for the [MeshCentral2](https://github.com/Ylianst/MeshCentral) Project.
+
+## Installation
+
+ Pre-requisite: First, make sure you have plugins enabled for your MeshCentral installation:
+>     "plugins": {
+>          enabled: true
+>     },
+Restart your MeshCentral server after making this change.
+
+ To install, simply add the plugin configuration URL when prompted:
+ `https://raw.githubusercontent.com/ryanblenis/MeshCentral-Sample/master/config.json`
+
+## Features
+Upon disconnecting from a remote desktop session, the user will be prompted to enter a note.
---
+++ sample.js
@@ -0,0 +1,25 @@
+/** 
+* @description MeshCentral Sample Plugin
+* @author Ryan Blenis
+* @copyright 
+* @license Apache-2.0
+* @version v0.0.1
+*/
+
+"use strict";
+
+module.exports.sample = function (parent) {
+    var obj = {};
+    obj.parent = parent; // keep a reference to the parent
+    obj.exports = [
+      "onDesktopDisconnect" // export this function to the web UI
+    ];
+    
+    obj.onDesktopDisconnect = function() {  // this is called when the desktop disconnect button is clicked
+        writeDeviceEvent(encodeURIComponent(currentNode._id));  // mimic what the button does on the device main page to pull up a log
+        Q('d2devEvent').text(Date().toLocaleString()+': '); // pre-fill the date for a timestamp
+        focusTextBox('d2devEvent');
+    }
+    
+    return obj;
+}
